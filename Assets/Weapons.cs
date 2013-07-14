5f9c47e34fbd59081f2f7c66d8fb0e202d24e801
Pyromuffin <kelly@pyromuffin.com>
2013-07-13 19:02:14 -0700

    I hope this works

--- Assets/Weapons.cs
+++ Assets/Weapons.cs
@@ -6,6 +6,7 @@ public class Weapons : MonoBehaviour {
 	public float readyTimer, readySpeed;
 	public GameObject sword;
 	private Vector3 startPos;
+	public float velocityMag;
 	
 	// Use this for initialization
 	void Start () {
@@ -15,9 +16,13 @@ public class Weapons : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+		velocityMag = rigidbody.velocity.magnitude;
+		
+		
 		if(Input.GetButtonDown("Fire1") && ready){
 			ready = false;
+			animation.Stop("idle");
 			gameObject.animation.Play("swing sword");
 			readyTimer = Time.time;
 			startPos = transform.position;
@@ -25,6 +30,20 @@ public class Weapons : MonoBehaviour {
 			
 		}
 		
+		if ( Mathf.Abs(Input.GetAxis("Horizontal")) > .1f ){
+			animation.Stop("idle");
+			animation.Blend("run");	
+			animation.Blend("arm jog",.3f);
+		}
+		else{
+			if(!animation.IsPlaying("swing sword") )
+			{
+				animation.Blend("idle",1f,1);
+			}
+			animation.Blend("run",0,1);
+			animation.Stop("arm jog");
+			
+		}
 		
 		if (animation.IsPlaying("swing sword") ){
 			sword.GetComponentInChildren<ParticleSystem>().emissionRate = 1000;
